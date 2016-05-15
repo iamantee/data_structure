3fa0ca3b49d1e85e4fd0d2663f9026b244a179ce
antee.ubuntu <iamantee@gmail.com>
2016-05-15 09:17:53 +0800

    #

--- list.c
+++ list.c
@@ -1,6 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* prototype */
 typedef struct node node;
 typedef struct list list;
 
+list listCreate();
+int push(void *pointer);
+(void*) iterator();
+
+/* structure */
 struct node{
   node * previous_node;
   node * next_node;
@@ -10,7 +19,35 @@ struct node{
 struct list{
   node * head_node;
   node * tail_node;
+
+  unsigned long length;
+
+  void (*push)(void *pointer);
+  void (*iterator)(void);
 };
+
+/* function implememtation */
+list listCreate(){
+  list *plist;
+
+  if(plist = malloc(sizeof(*plist)) == NULL) return NULL;
+
+  plist->head = plist->tail = NULL;
+
+  plist->length = 0;
+
+  plist->push = push;
+  plist->iterator = iterator;
+
+  return plist;
+}
+
+int push(void *pointer){
+  if(plist == NULL) return 1;
+  
+  node *pnode;
+
+}
 
 int main(int argc, char** argv){
   return 0;
