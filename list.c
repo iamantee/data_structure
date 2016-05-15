10a75d9556a89f20f7d608b08b4622cb4d5f2a2e
Antee <iamantee@gmail.com>
2016-05-15 08:09:30 +0000

    add func implememtation in progress

--- list.c
+++ list.c
@@ -11,7 +11,8 @@ typedef struct node node;
 typedef struct list list;
 
 list listCreate();
-int push(void *pointer);
+node nodeCreate();
+int push(list *plist, void *value);
 (void*) iterator();
 
 /* structure */
@@ -32,10 +33,37 @@ struct list{
 };
 
 /* function implememtation */
+
+/* *
+ * Description:
+ * nodeCreate function is to init a new node.
+ *
+ * Return Value:
+ * nodeCreate function return the inited node.
+ * */
+node nodeCreate(){
+  node *pnode;
+
+  if(pnode = malloc(sizeof(*node)) == NULL) return NULL;
+
+  pnode->previous_node = NULL;
+  pnode->next_node = NULL;
+  pnode->value = NULL;
+
+  return pnode;
+}
+
+/* *
+ * Description:
+ * listCreate function is to init a new linked list.
+ *
+ * Return Value:
+ * listCreate function return the inited linked list.
+ * */
 list listCreate(){
   list *plist;
 
-  if(plist = malloc(sizeof(*plist)) == NULL) return NULL;
+  if(plist = malloc(sizeof(*list)) == NULL) return NULL;
 
   plist->head = plist->tail = NULL;
 
@@ -47,11 +75,37 @@ list listCreate(){
   return plist;
 }
 
-int push(void *pointer){
+/* *
+ * Description: 
+ * push function is to push a new value into the specified linked list on the tail.
+ * When the length of the linked list is 0, the value of head node and tail node are the same.
+ * Although the number of nodes is 2 at this specific situation, the length of list is set to be 1. 
+ *
+ * Return Value:
+ * push function returns 0 to refer to success and 1 for failure.
+ * */
+int push(list *plist, void *value){
   if(plist == NULL) return 1;
   
-  node *pnode;
+  node *pnode = nodeCreate();
+  pnode->value = value;
 
+  if(plist->length == 0){
+    plist->head_node = plist->tail_node = pnode;
+    plist->length = 1;
+  } else if(plist->length == 1){
+    plist->tail_node = pnode;
+    plist->head_node->next_node = plist->tail_node;
+    plist->tail_node->previous_node = plist->head_node;
+    plist->length = 2;
+  } else{
+    plist->tail_node->next_node = pnode;
+    pnode->previous_node = plist->tail_node;
+    plist->tail_node = pnode;
+    ++(plist->length);
+  }
+
+  return 0;
 }
 
 int main(int argc, char** argv){
